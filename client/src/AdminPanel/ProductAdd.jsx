c8cfd17a6eb98b90e983d567b84b1f571cb4270a
Biswojit Biswal <biswojitb474@gmail.com>
2024-11-21 22:00:35 +0530

    debugging

--- client/src/AdminPanel/ProductAdd.jsx
+++ client/src/AdminPanel/ProductAdd.jsx
@@ -87,6 +87,7 @@ function ProductAdd() {
       });
 
       const data = await response.json();
+      console.log(data);
 
       if (response.ok) {
         toast.success("Products Added Successfully");
