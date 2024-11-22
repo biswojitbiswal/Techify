6784a42c280e7b373a64d835443996b56efa3464
Biswojit Biswal <biswojitb474@gmail.com>
2024-11-22 21:34:08 +0530

    fixing add product bug

--- server/middleware/multer.middleware.js
+++ server/middleware/multer.middleware.js
@@ -1,20 +1,4 @@
-// import express from 'express';
-// import multer from 'multer'
-
- 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, "./public/temp")
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//       cb(null, file.originalname + '-' + uniqueSuffix)
-//     }
-//   })
-  
-// export const upload = multer({ 
-//     storage, 
-// })
+import express from 'express';
 import multer from 'multer'
 
  
@@ -24,7 +8,7 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.originalname + '-' + uniqueSuffix);
+      cb(null, file.originalname + '-' + uniqueSuffix)
     }
   })
   
